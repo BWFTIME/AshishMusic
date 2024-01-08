bb8c68b51b431d0a6a8cec963387bbb463629802
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-01-08 09:34:24 +0530

    Update Youtube.py

--- AshishX/platforms/Youtube.py
+++ AshishX/platforms/Youtube.py
@@ -31,7 +31,7 @@ class YouTubeAPI:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
         self.regex = r"(?:youtube\.com|youtu\.be)"
-        self.status = "https://www.youtube.com/oembed?url="
+        self.status = "https://www.youtube.com/oembed.url="
         self.listbase = "https://youtube.com/playlist?list="
         self.reg = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
 
