46dbc205c8c57ea8d6f8ffccaa6676de4b8d2dd7
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-01-08 15:31:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFTIME/AshishXMusic",
+    "https://github.com/BWFTIME/AshishMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bwf")
 GIT_TOKEN = getenv(
