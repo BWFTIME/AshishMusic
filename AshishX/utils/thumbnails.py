ffb2f459c111b14be470b32ea2b60f0e47d2b7c1
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-01-09 13:21:05 +0530

    Update thumbnails.py

--- AshishX/utils/thumbnails.py
+++ AshishX/utils/thumbnails.py
@@ -123,7 +123,7 @@ async def get_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text((10, 10), "ASHISH MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "L2R PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
